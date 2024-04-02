c12465f68a73da2f08a601af5e93e9553b810615
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-04-02 22:30:44 +0200

    ppd-driver-intel-pstate: Do not load logind properties or autostart it

--- src/ppd-driver-intel-pstate.c
+++ src/ppd-driver-intel-pstate.c
@@ -191,7 +191,8 @@ probe_epb (PpdDriverIntelPstate *pstate)
   }
 
   pstate->logind_proxy = g_dbus_proxy_new_for_bus_sync (G_BUS_TYPE_SYSTEM,
-                                                        0,
+                                                        G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START |
+                                                        G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES,
                                                         NULL,
                                                         SYSTEMD_DBUS_NAME,
                                                         SYSTEMD_DBUS_PATH,
